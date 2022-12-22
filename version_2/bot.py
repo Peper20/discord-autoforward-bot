b2befd83725ba2cccf84e0dfb4bddf9c294e9753
Ivan Baluk <ivan.baluk@bk.ru>
2022-12-22 19:24:44 +0200

    discord autoforward bot v2

--- version_2/bot.py
+++ version_2/bot.py
@@ -1,6 +1,9 @@
 import discord
+import os
+import requests
 
 
+from PIL import Image
 from asyncio import create_task
 
 
@@ -25,8 +28,21 @@ class My_bot(discord.Bot):
 	async def get_simple_id(place):
 		return place.id
 
+	__ids_handlers = {
+		discord.User: get_user_dm_id,
+		discord.TextChannel: get_simple_id,
+	}
 
-	__ids_handlers = {discord.User: get_user_dm_id, discord.TextChannel: get_simple_id}
+	@staticmethod
+	async def save_file(url, index):
+		data = requests.get(url, stream=True)
+		name = f'trash_imgs/{index}.{url.split(".")[-1]}'
+
+		with open(name, 'wb') as file:
+			file.write(data.content)
+
+		return name
+
 
 	async def get_place_id(self, place):
 		return await self.__ids_handlers[type(place)](place)
@@ -64,7 +80,28 @@ class My_bot(discord.Bot):
 
 		print(f'Bot connected as user {self.user} (id {self.user.id})')
 
+
 	async def on_message(self, message):
 		if message.channel.id in self._places_ids_from_forward and not message.author.bot:
+			
+			get_files_tasks = []
+			for index, attachment in enumerate(message.attachments):
+				get_files_tasks.append(
+					create_task(
+						self.save_file(attachment.url, index),
+					)
+				)
+
+			send_messages_tasks = []
 			for place in self._places_to_forward:
-				await place.send(message.content)
+				send_messages_tasks.append(
+					create_task(
+						place.send(
+							message.content,
+							files=[discord.File(await file_name) for file_name in get_files_tasks],
+						)
+					)
+				)
+
+			for task in send_messages_tasks:
+				await task
